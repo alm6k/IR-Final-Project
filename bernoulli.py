462a34a61caf483afc377e21d9a1f9da7da44407
Anthony M <antonius6k+github@gmail.com>
2014-05-04 22:00:33 -0500

    Added skeleton files for each of the programs to write

---
+++ bernoulli.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
+# code for the Multivariate Bernoulli Naive Bayes classifier
+# remember to make it a method (call it 'classify' or something?) that can be called when this module is imported into the evaluation file
+
+def classify():
+    print "hello, bernoulli"
+
+if __name__ == "__main__":
+    classify()
---
+++ cross-validate.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import os
+
+# code for the ten-fold cross validator being used to evaluate the classifiers
+# this code will call the "classify" method for each group
+# and display the average accuracy for both classifiers
