95e2a7c8e6de0eb900f98d7f5cbbf05f058f14aa
Matt Houglum <mhoug89@gmail.com>
2014-05-06 21:07:15 -0500

    Wrote train function for multinomial

--- multinomial.py
+++ multinomial.py
@@ -5,8 +5,24 @@ import os
 # code for the Multinomial Naive Bayes classifier
 # remember to make it a method (call it 'classify' or something?) that can be called when this module is imported into the evaluation file
 
-def classify():
-    print "hello, multinomial"
+def train_multinomial(master_dict, emails):
+    # Initialize all counts to 0, as this function could be called more
+    # than once, and should count up from 0
+    for t in master_dict['termCounts']
+        master_dict['termCounts'][t] = [ [0,0], [0,0], [0,0], [0,0] ]
 
-if __name__ == "__main__":
-    classify()
+    for e in emails:
+        # save the index where the count gets added
+        i = e['truthCategory']
+
+        for t in e['terms']:
+            # use index 1 at the end here to get the 2nd element of the list
+            # since we're doing the multinomial count
+            master_dict['termCounts'][t][i][1] += 1
+#END train_multinomial()
+
+
+def classify_multinomial(master_dict, emails):
+    #TODO: write this
+    pass
+
