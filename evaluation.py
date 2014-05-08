1c2e5a1d63c40c35b5ecf9254caa94ce5733a908
Anthony L Mills <alm6k@mtmail.mtsu.edu>
2014-05-07 21:22:04 -0500

    Did some formatting magic on the evaluation script

--- evaluation.py
+++ evaluation.py
@@ -18,7 +18,8 @@ pp = pprint.PrettyPrinter(indent=4)
 # parse the emails
 master_dict = p.parse_emails()
 
-print master_dict['classCount']
+print "# emails per class:", master_dict['classCount']
+print "Vocabulary size: %d" % master_dict['vocabSize']
 
 # average successful classification rate for the ten-fold CV
 bernAvg = 0.0
@@ -32,9 +33,9 @@ multRandAvg = 0.0
 groups = [master_dict['emails'][i::10] for i in xrange(10)]
 
 # test each group against the other groups
-for i in xrange(len(groups)):
+for i in xrange(1, len(groups)+1):
     # test group is the ith group
-    testGroup = groups[i]
+    testGroup = groups[i-1]
     testTruths = [k['truthCategory'] for k in testGroup]
     testSize = len(testTruths)
 
@@ -44,13 +45,13 @@ for i in xrange(len(groups)):
 
     # training group is all the other groups
     # flattened into one list
-    trainGroup = list(it.chain.from_iterable(groups[0:i] + groups[i+1:10]))
+    trainGroup = list(it.chain.from_iterable(groups[0:i-1] + groups[i:10]))
 
     # train with the training group
     b.train(master_dict, trainGroup)
     m.train(master_dict, trainGroup)
 
-    print "\n-----"
+    print "\n\033[1;32m----- TEST GROUP %d -----\033[0m" % i
     # classify each item in the test group
     for item in testGroup:
         testBClass.append(b.classify(master_dict, item))
@@ -62,7 +63,9 @@ for i in xrange(len(groups)):
     print "m-class:", testMClass
 
     # compute the score for this iteration
+    print "\033[1;33mBern\033[0m accuracy: %.4f" % (1.0*sum([x[0] == x[1] for x in it.izip(testTruths, testBClass)]) / testSize)
     bernAvg += 1.0*sum([x[0] == x[1] for x in it.izip(testTruths, testBClass)]) / testSize
+    print "\033[1;31mMult\033[0m accuracy: %.4f" % (1.0*sum([x[0] == x[1] for x in it.izip(testTruths, testMClass)]) / testSize)
     multAvg += 1.0*sum([x[0] == x[1] for x in it.izip(testTruths, testMClass)]) / testSize
 
     """
@@ -90,8 +93,9 @@ for i in xrange(len(groups)):
         elif t[0] != t[1] and c[0] != c[1]:
             D += 1
 
-    print "Bern RI vals:", A, B, C, D
+    #print "Bern RI vals for %d:" % i, A, B, C, D
     try:
+        print "\033[1;33mBern\033[0m RI score: %.4f" % (1.0*(A+D)/(A+B+C+D))
         bernRandAvg += 1.0*(A+D)/(A+B+C+D)
     except:
         pass
@@ -114,19 +118,20 @@ for i in xrange(len(groups)):
         elif t[0] != t[1] and c[0] != c[1]:
             D += 1
 
-    print "Mult RI vals:", A, B, C, D
+    #print "Mult RI vals for %d:" % i, A, B, C, D
     try:
+        print "\033[1;31mMult\033[0m RI score: %.4f" % (1.0*(A+D)/(A+B+C+D))
         multRandAvg += 1.0*(A+D)/(A+B+C+D)
     except:
         pass
 
 
 # print the results
-print "\n\n*** Results ***\n-----"
-print "  Mean Bernoulli Accuracy:   %.2f" % (bernAvg / 10.0)
-print "Mean Multinomial Accuracy:   %.2f" % (multAvg / 10.0)
+print "\n\n\033[1;35m*** Results ***\033[0m\n-----"
+print "Mean \033[1;33mBern\033[0m Accuracy:   %.4f" % (bernAvg / 10.0)
+print "Mean \033[1;31mMult\033[0m Accuracy:   %.4f" % (multAvg / 10.0)
 
 print "-----"
-print "  Mean Bernoulli Rand Index: %.2f" % (bernRandAvg / 10.0)
-print "Mean Multinomial Rand Index: %.2f" % (multRandAvg / 10.0)
+print "Mean \033[1;33mBern\033[0m Rand Index: %.4f" % (bernRandAvg / 10.0)
+print "Mean \033[1;31mMult\033[0m Rand Index: %.4f" % (multRandAvg / 10.0)
 
